598c03300c77ca530d1d7e4781b749b00fdff07d
smitarevolutions <smita.revolutions@gmail.com>
2024-05-15 16:33:18 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,4 +38,7 @@ if ingredients_list:
         st.success('Your Smoothie is ordered!')
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-st.text(fruityvice_response)
+
+fvdf = st.dataframe()
+#st.text(fruityvice_response.json())
+fv_df = st.dataframe(data=fruityvice_response.json(),use_container_width=True)
